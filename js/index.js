5e0e7c703be8ce32db10d14924e10880a155629f
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-08-21 05:50:29 -0700

    Getting input from user

--- js/index.js
+++ js/index.js
@@ -1,10 +1,34 @@
 function myFunction(clickdID) {
+  // Change or unchange the Three Selection row 
   let theStyle = document.getElementById(clickdID).style.backgroundColor;
   if(theStyle === "yellow"){
     let highlight = document.getElementById(clickdID);
-  highlight.style.backgroundColor="gray";
+  highlight.style.backgroundColor="transparent";
   }else{
     let highlight = document.getElementById(clickdID);
   highlight.style.backgroundColor="yellow";
   }
 } 
+
+
+function createCard(){
+  let cat = document.getElementById("category");
+  let title = document.getElementById("title");
+  let descrip = document.getElementById("descrip");
+  let URl= document.getElementById("URl");
+  let socialM = document.getElementById("socialM");
+  
+  // The input values of the input text
+  let Category = cat.value;
+  let Title = title.value;
+  let Description = descrip.value;
+  let URL = URl.value;
+  let SocailMedia = socialM.value;
+  
+ 
+    document.getElementById("test").innerHTML = "category"+Category
+    + " title " + Title + " Description "+Description + " URL "
+    +URL + " " + SocailMedia ;
+
+}
+
