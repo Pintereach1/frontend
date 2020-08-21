f4ee01246cf3a9f1ec3440158e6af07749b6bfec
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-08-21 03:14:47 -0700

    Put in edit and delete popover buttons

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
+function myFunction(clickdID) {
+  let theStyle = document.getElementById(clickdID).style.backgroundColor;
+  if(theStyle === "yellow"){
+    let highlight = document.getElementById(clickdID);
+  highlight.style.backgroundColor="gray";
+  }else{
+    let highlight = document.getElementById(clickdID);
+  highlight.style.backgroundColor="yellow";
   }
 } 
