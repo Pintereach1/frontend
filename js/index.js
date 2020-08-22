b965a668bd645d7cf216072c9b4deba4d93340a3
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-08-22 03:06:48 -0700

    Add in Edit and Delete Buttons

--- js/index.js
+++ js/index.js
@@ -1,12 +1,58 @@
+class cardInput{
+  constructor(titl,cat,desc,url,social){
+    this.Title = titl;
+    this.Category = cat;
+    this.Description = desc;
+    this.URL = url;
+    this.Social  = social;
+  }
+   
+}
+
+
+
+function addElement (idClkd) {
+ // document.body.onload = addElement; 
+  // create a new div element 
+  let newDiv = document.createElement("div"); 
+  // new buttons 
+  let DeleteBtn = document.createElement("BUTTON");   // Create a <button> element
+  DeleteBtn.innerHTML = "Delete";     
+  let EditBtn = document.createElement("button");   // Create a <button> element
+  EditBtn.innerHTML = "Edit";               // Insert text
+  // and give it some content 
+  let newContent = document.createTextNode("Hi there and greetings!"); 
+  // add the text node to the newly created div
+  //newDiv.appendChild(newContent);  
+
+
+
+  //let addin  = document.querySelector(".crd");
+
+  // add the newly created element and its content into the DOM 
+
+  let currentDiv = document.querySelector(idClkd);
+  let addin  = currentDiv.querySelectorAll(".crd"); 
+  addin.appendChild(EditBtn);
+  addin.appendChild(DeleteBtn);
+  addin.appendChild(EditBtn);
+  addin.appendChild(DeleteBtn);
+  // document.body.insertBefore(currentDiv, addin); 
+}
+
 function myFunction(clickdID) {
   // Change or unchange the Three Selection row 
-  let theStyle = document.getElementById(clickdID).style.backgroundColor;
+    let theStyle = document.getElementById(clickdID).style.backgroundColor;
   if(theStyle === "yellow"){
     let highlight = document.getElementById(clickdID);
   highlight.style.backgroundColor="transparent";
   }else{
     let highlight = document.getElementById(clickdID);
-  highlight.style.backgroundColor="yellow";
+    highlight.style.backgroundColor="yellow";
+    
+    addElement(clickdID);
+
+
   }
 } 
 
@@ -18,17 +64,54 @@ function createCard(){
   let URl= document.getElementById("URl");
   let socialM = document.getElementById("socialM");
   
-  // The input values of the input text
-  let Category = cat.value;
-  let Title = title.value;
-  let Description = descrip.value;
-  let URL = URl.value;
-  let SocailMedia = socialM.value;
-  
+  let Category = '';
+  let Title = '';
+  let Description = '';
+  let Url = '';
+  let SocailMedia = '';
+      // The input values of the input text
+  if(cat === null){
+    // Category = cat.default;
+  }else {
+    Category= cat.value;
+  }
+  if(title === null){
+    // Title = title.default;
+  } else{
+    Title = title.value;
+  }
+  if(descrip === null){
+
+  }else{
+  Description = descrip.value;
+  }
+  if(URl === null){
+
+  }else{
+    Url = URl.value;
+  }
+  if(socialM === null){
+
+  }else{
+    SocailMedia = socialM.value;
+  }
+
+
  
-    document.getElementById("test").innerHTML = "category"+Category
-    + " title " + Title + " Description "+Description + " URL "
-    +URL + " " + SocailMedia ;
+  // All the user input into a class with properties
+  let uI = new cardInput(Title,Category,Description,Url,SocailMedia);
+  console.log(uI.length);
+  let re = [{"Category":uI.Category,"Title":uI.Title,"Description":uI.Description
+      , "URL":uI.URL,"Social":uI.Social}];
+  return re;
+    // document.getElementById("test").innerHTML = "category"+uI.Category
+    // + " title " + uI.Title + " Description "+uI.Description + " URL "
+    // +uI.URL + " " + uI.Social ;
+  
 
 }
+
+let userNn = new cardInput('f','f','s','a','s');
+let userN = createCard();
+console.log('str'+userN.Category);
 
